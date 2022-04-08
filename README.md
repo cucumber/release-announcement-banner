886a1a2723c362bc521c063475b02f2059faad38
David Goss <david@davidgoss.co>
2022-04-08 10:35:27 +0100

    add automation

--- README.md
+++ README.md
@@ -4,11 +4,7 @@
 
 ## Usage
 
-1. Swap in the name and version of what's been released
-2. Open in a browser
-3. Screenshot and trim whitespace
-
-## Todo
-
-- Automate screenshot with puppeteer
-- Add a CLI
+```shell
+$ npm install
+$ npm run generate
+```
---
+++ index.js
@@ -0,0 +1,38 @@
+import inquirer from 'inquirer'
+import ora from 'ora'
+import chalk from 'chalk'
+import puppeteer from 'puppeteer'
+
+const { tool, version } = await inquirer.prompt([
+    {
+        name: 'tool',
+        type: 'string',
+        message: "What's the name of the tool we've released?",
+        default: 'cucumber-ruby'
+    },
+    {
+        name: 'version',
+        type: 'string',
+        message: "Which version that we've released?",
+        default: 'v1.2.3'
+    }
+])
+
+const spinner = ora({
+    color: 'blue',
+    text: 'Generating your announcement banner...'
+}).start()
+
+const url = new URL('./index.html', import.meta.url)
+url.searchParams.append('tool', tool)
+url.searchParams.append('version', version)
+const browser = await puppeteer.launch()
+const page = await browser.newPage()
+await page.goto(url.toString())
+await page.waitForSelector('.is-finished', { timeout: 1000 })
+const element = await page.$('main')
+await element.screenshot({ path: './screenshot.png' })
+await page.close()
+await browser.close()
+
+spinner.succeed(`Screenshot saved to ${chalk.bgGray.whiteBright('screenshot.png')}!`)
